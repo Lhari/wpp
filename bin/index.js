9b6d4e5e3e77e7859e28457ae1990999e00167e9
Kasper <kashan7@live.dk>
2023-04-22 23:42:35 +0200

    make version functional

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,15 @@
 #! /usr/bin/env node
-let fs = require("fs");
-const package = require('../package.json');
+import chalk from 'chalk';
+import * as fs from 'fs'
+import * as path from 'path'
 
-// import the generateConfiguration function from create-wpkg.js
-const generateConfiguration = require("./create-wpkg");
+import { table } from './modules/table.mjs'
+import { generateConfiguration } from './modules/create-wpkg.mjs'
+
+const __dirname = path.resolve();
+
+// read the package.json file
+const pck = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')));
 
 // Get current directory
 const currentLocation = process.cwd();
@@ -13,75 +19,90 @@ const folderName = currentLocation.includes('\\') ? currentLocation.split("\\").
 const args = process.argv.slice(2);
 let wpPackage
 
-
+// If the API is run and file doesn't exist, start out by forcing the file to be created.
 if(!fs.existsSync(`${currentLocation}/wpkg.json`)) {
     generateConfiguration(folderName);
 } else {
     wpPackage = JSON.parse(fs.readFileSync(`${currentLocation}/wpkg.json`));
-}
+    // read the contents of the wpkg.json file based on currentLocation, if the file exists
 
+    switch(args[0]) {
+        case '-v':
+        case '--version':
+        case 'version':
 
+            if(args[1] === '--new-version') {
 
-// read the contents of the wpkg.json file based on currentLocation, if the file exists
+                if(args[2].match(/^[0-9]+\.[0-9]+\.[0-9]+$/) && args[2] !== wpPackage.version) {
+                    // update the version number within the package.json file
+                    wpPackage.version = args[2]
+                    fs.writeFileSync(path.join(__dirname, 'wpkg.json'), JSON.stringify(wpPackage, null, 2))
+                } else {
+                    console.log(chalk.redBright('Please provide a valid version number'));
 
-switch(args[0]) {
-    case '-v':
-    case '--version':
-    case 'version':
+                }
+            } else {
+                // get the version from wpkg.json
+                console.log(chalk.blueBright(`${chalk.cyanBright(wpPackage.version)}`))
 
-        // get the version from package.json
-        console.log(package.version);
-        break
+            }
 
-    case 'install':
-    case 'i':
-        // If package is given, install the specific package, else loop over all dependencies and install them
-        break
+            
+            break
 
-    case 'remove':
-    case 'r':
+        case 'install':
+        case 'i':
+            // If package is given, install the specific package, else loop over all dependencies and install them
+            break
 
-        // If remove is set, require a package to be given. Delete package from wpkg.json, then remove the package from the WP install
-        console.log('remove package')
-        break
+        case 'remove':
+        case 'r':
 
-    case 'upgrade':
-    case 'u':
+            // If remove is set, require a package to be given. Delete package from wpkg.json, then remove the package from the WP install
+            console.log('remove package')
+            break
 
-        // If no package is given, simply upgrade all packages that aren't up to the version set within wpkg.json
+        case 'upgrade':
+        case 'u':
 
-        // if a package is given, extract the package name and version number from the process.argv array, update it within wpkg.json and install it
-        console.log('update package, if no package is given, update all packages')
-        break
-    
-    case 'list':
-    case 'l':
-    
-        // List all dependencies, and their versions    
-        let dependencyObj = []
-        for(let [key, value] of (Object.entries(wpPackage.dependencies))) {
-            dependencyObj.push({name: key, version: value})
-        }
+            // If no package is given, simply upgrade all packages that aren't up to the version set within wpkg.json
 
-        // TODO: Add Chalk to make this look better
-        console.log('')
-        console.log(`Listing all dependencies (${dependencyObj.length})`)
-        console.table(dependencyObj)
-        break
-
-    case 'help':
-    case 'h':
-    case '-h':
-    case '--help':
-
-        // Write out a helpful message, explaining how to utilize the full API
-        console.log('help')
-        break
-
-    default:
-        // Simply just warn people that they cannot use the command without some sort of parameter, after the first time.
-        console.log('Unknown command, use -h or --help for help')
+            // if a package is given, extract the package name and version number from the process.argv array, update it within wpkg.json and install it
+            console.log('update package, if no package is given, update all packages')
+            break
         
-        break;
+        case 'list':
+        case 'l':
+        
+            // List all dependencies, and their versions    
+            let dependencyObj = []
+            for(let [key, value] of (Object.entries(wpPackage.dependencies))) {
+                dependencyObj.push({name: key, version: value})
+            }
+
+            // TODO: Add Chalk to make this look better
+            console.log('')
+            console.log(chalk.blueBright(`Listing all dependencies (${chalk.cyanBright(dependencyObj.length)})`))
+            table(dependencyObj)
+            break
+
+        case 'help':
+        case 'h':
+        case '-h':
+        case '--help':
+
+            // Write out a helpful message, explaining how to utilize the full API
+            console.log('help')
+            break
+
+        default:
+            // Simply just warn people that they cannot use the command without some sort of parameter, after the first time.
+            console.log('Unknown command, use -h or --help for help')
+            
+            break;
+    }
 }
+
+
+
 
--- bin/modules/create-wpkg.mjs
+++ bin/modules/create-wpkg.mjs
@@ -1,6 +1,9 @@
 // add readline
-const readline = require('readline');
-const fs = require('fs');
+import * as readline from 'readline';
+import * as fs from 'fs';
+
+import chalk from 'chalk'
+
 const baseObj = {
     "name": "",
     "version": "1.0.0",
@@ -11,7 +14,7 @@ const baseObj = {
 
 
 
-  module.exports = function generateConfiguration(name) {
+export function generateConfiguration(name) {
 
     // ask a readline question for each entry of the baseObj, if the answer is empty, use the default value
     // if the answer is not empty, use the answer
@@ -21,29 +24,29 @@ const baseObj = {
         output: process.stdout
     });
     
-    rl.question(`project name: (${name})`, (answer) => {
+    rl.question(chalk.blueBright(`project name: (${chalk.cyanBright(name)})`), (answer) => {
         if (answer !== '') {
             baseObj.name = answer;
         }
-        rl.question(`version: (${baseObj.version})`, (answer) => {
+        rl.question(chalk.blueBright(`version: (${chalk.cyanBright(baseObj.version)})`), (answer) => {
             if (answer !== '') {
                 baseObj.version = answer;
             }
-            rl.question('description: ', (answer) => {
+            rl.question(chalk.blueBright('description: '), (answer) => {
                 baseObj.description = answer;
-                rl.question('author: ', (answer) => {
+                rl.question(chalk.blueBright('author: '), (answer) => {
                     baseObj.author = answer;
 
                     // Ask if this is ok
 
-                    rl.question(`Is this ok? `, (answer) => {
+                    rl.question(chalk.blueBright(`Is this ok? `), (answer) => {
                         answer = answer.toLowerCase()
                         if (answer === 'y' || answer === 'yes') {
                             writeFile();
                         } else if(answer === 'n' || answer === 'no') {
-                            console.log('Aborting');
+                            console.log(chalk.redBright('Config file was not created, please run the API again'));
                         } else {
-                            console.log('Invalid answer');
+                            console.log(chalk.redBright('Please answer yes or no'));
                         }
 
                     rl.close();
@@ -62,7 +65,7 @@ baseObj.dependencies = {}
 
 fs.writeFile('wpkg.json', JSON.stringify(baseObj, null, 2), (err) => {
     if (err) throw err;
-    console.log('The file has been saved!');
+    console.log(chalk.greenBright('The configuration has been saved!'));
 });
 
 }
